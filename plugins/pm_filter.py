1e0e458e2b8139bfa9bcb36d5897bf1aecaa5aa1
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2023-07-04 18:53:15 +0530

    Added manual fltr

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -935,3 +935,69 @@ async def advantage_spell_chok(msg):
         await message.delete()
     except:
         pass
+
+async def manual_filters(client, message, text=False):
+    group_id = message.chat.id
+    name = text or message.text
+    reply_id = message.reply_to_message.id if message.reply_to_message else message.id
+    keywords = await get_filters(group_id)
+    for keyword in reversed(sorted(keywords, key=len)):
+        pattern = r"( |^|[^\w])" + re.escape(keyword) + r"( |$|[^\w])"
+        if re.search(pattern, name, flags=re.IGNORECASE):
+            reply_text, btn, alert, fileid = await find_filter(group_id, keyword)
+
+            if reply_text:
+                reply_text = reply_text.replace("\\n", "\n").replace("\\t", "\t")
+
+            if btn is not None:
+                try:
+                    if fileid == "None":
+                        if btn == "[]":
+                            dm =await client.send_message(
+                                group_id, 
+                                reply_text, 
+                                disable_web_page_preview=True,
+                                reply_to_message_id=reply_id)
+                            await asyncio.sleep(30)
+
+                            await dm.delete()
+
+                            await message.delete()
+                        else:
+                            button = eval(btn)
+                            dm= await client.send_message(
+                                group_id,
+                                reply_text,
+                                disable_web_page_preview=True,
+                                reply_markup=InlineKeyboardMarkup(button),
+                                reply_to_message_id=reply_id
+                            )
+                            await asyncio.sleep(30)
+                            await dm.delete()
+                            await message.delete()
+                    elif btn == "[]":
+                        dm= await client.send_cached_media(
+                            group_id,
+                            fileid,
+                            caption=reply_text or "",
+                            reply_to_message_id=reply_id
+                        )
+                        await asyncio.sleep(30)
+                        await dm.delete()
+                        await message.delete()
+                    else:
+                        button = eval(btn)
+                        dm= await message.reply_cached_media(
+                            fileid,
+                            caption=reply_text or "",
+                            reply_markup=InlineKeyboardMarkup(button),
+                            reply_to_message_id=reply_id
+                        )
+                        await asyncio.sleep(30)
+                        await dm.delete()
+                        await message.delete()
+                except Exception as e:
+                    logger.exception(e)
+                break
+    else:
+        return False
